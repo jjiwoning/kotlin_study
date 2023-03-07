e904ee65297e37ebae5fba5f938319520a728ad1
JJiWoNing <jjiwoning12@gmail.com>
2023-03-07 09:46:02 +0900

    3/7 Study

---
+++ src/main/kotlin/algo/P1085/Main.kt
@@ -0,0 +1,19 @@
+package algo.P1085
+
+import java.util.StringTokenizer
+import kotlin.math.min
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val st = StringTokenizer(readLine())
+
+    val x = st.nextToken().toInt()
+    val y = st.nextToken().toInt()
+    val w = st.nextToken().toInt()
+    val h = st.nextToken().toInt()
+
+    var answer : Int = min(x, y)
+    answer = min(answer, w - x)
+    answer = min(answer, h - y)
+
+    println(answer)
+}
