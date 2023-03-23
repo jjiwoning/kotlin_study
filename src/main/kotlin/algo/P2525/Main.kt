7d68cc1a535387e3c22fc4efb9e73595b251db80
JJiWoNing <jjiwoning12@gmail.com>
2023-03-24 08:42:07 +0900

    3/24 Study

---
+++ src/main/kotlin/algo/P2525/Main.kt
@@ -0,0 +1,17 @@
+package algo.P2525
+
+fun main(args: Array<String>) {
+    val (h, m) = readln().split(" ").map { it.toInt() }
+    val time = readln().toInt()
+    val totalTime = 60 * h + m + time
+
+    var answerHour : Int = totalTime / 60
+    if (answerHour >= 24) {
+        answerHour -= 24;
+    }
+    val answerMin = totalTime % 60
+
+    print(answerHour)
+    print(" ")
+    print(answerMin)
+}
