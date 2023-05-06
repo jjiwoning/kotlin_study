e5a271da4a5aca8f5b7a51be30682de85494acb8
JJiWoNing <jjiwoning12@gmail.com>
2023-05-06 15:56:50 +0900

    5/6 study

---
+++ src/main/kotlin/algo/programmers/P181876/Solution.kt
@@ -0,0 +1,9 @@
+package algo.programmers.P181876
+
+import java.util.*
+
+class Solution {
+    fun solution(myString: String): String {
+        return myString.lowercase(Locale.getDefault())
+    }
+}
