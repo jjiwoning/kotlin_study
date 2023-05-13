19ce7c200f2b6ea0f57c03a39c18c1102f82f594
JJiWoNing <jjiwoning12@gmail.com>
2023-05-13 17:35:17 +0900

    5/13 study

---
+++ src/main/kotlin/algo/programmers/P181937/Solution.kt
@@ -0,0 +1,10 @@
+package algo.programmers.P181937
+
+class Solution {
+    fun solution(num: Int, n: Int): Int {
+        if (num % n == 0) {
+            return 1;
+        }
+        return 0;
+    }
+}
