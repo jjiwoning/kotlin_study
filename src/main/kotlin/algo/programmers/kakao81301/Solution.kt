ab9fa0bf327ae1022bd8977bdc8ddc3e55baee3c
JJiWoNing <jjiwoning12@gmail.com>
2023-10-25 16:58:21 +0900

    kakao 기출 숫자 문자열과 영단어

---
+++ src/main/kotlin/algo/programmers/kakao81301/Solution.kt
@@ -0,0 +1,32 @@
+package algo.programmers.kakao81301
+
+class Solution {
+
+    var map: MutableMap<String, String> = HashMap()
+
+    fun solution(s: String): Int {
+        initMap()
+        return replaceAllStringToNumber(s)
+    }
+
+    private fun replaceAllStringToNumber(s: String): Int {
+        var replaceString: String = s
+        for (key in map.keys) {
+            replaceString = replaceString.replace(key, map[key].toString())
+        }
+        return replaceString.toInt()
+    }
+
+    private fun initMap() {
+        map["zero"] = "0"
+        map["one"] = "1"
+        map["two"] = "2"
+        map["three"] = "3"
+        map["four"] = "4"
+        map["five"] = "5"
+        map["six"] = "6"
+        map["seven"] = "7"
+        map["eight"] = "8"
+        map["nine"] = "9"
+    }
+}
