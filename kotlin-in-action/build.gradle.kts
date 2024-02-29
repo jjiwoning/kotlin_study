02cc9f472997bb04c89e00a78346839a9226d61b
JJiWoNing <jjiwoning12@gmail.com>
2024-02-29 15:19:51 +0900

    chore: 코루틴 추가

--- kotlin-in-action/build.gradle.kts
+++ kotlin-in-action/build.gradle.kts
@@ -13,6 +13,7 @@ repositories {
 
 dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
 }
 
 tasks.test {
